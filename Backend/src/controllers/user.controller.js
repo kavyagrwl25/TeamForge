72c13b977084a29b7d315c6611d07ec766c2f946
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-04-04 20:18:56 +0530

    feat: enhance user registration with async handling and detailed validation checks

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,16 +1,41 @@
-import { AsyncHandler } from "../utils/AsyncHandler"
-import User from "../models/user.model.js"
+import { AsyncHandler } from "../utils/AsyncHandler"             // for named export, import with braces
+import { ApiError } from "../utils/ApiError"
+import { ApiResponse } from "../utils/ApiResponse"
+import { User } from "../models/user.model"                
 import { isValidFullName, isValidEmail, isValidPassword, isValidUserName } from "../utils/validators"
 
-const register = AsyncHandler( (req, res) => {
+const register = AsyncHandler( async (req, res) => {
     // 1. POST /users
     // 2. get details from user
     // 3. validate them and also check if user already exists with userName and email
     // 4. create a document in db and store the user 
     // 5. store this in an obj userCreated without selecting userPassword/RefreshToken
     // 6. return this object in data field in response
-
     const { fullName, userName, email, password } = req.body 
-    
-    // validate all essential entities
+    if(!fullName || !isValidFullName(fullName)){
+        throw new ApiError(400, "Full name is required, Please enter valid full name")
+    }
+    if(!userName || !isValidUserName(userName)){
+        throw new ApiError(400, "User name is required, Please enter valid user name")
+    }
+    if(!email || !isValidEmail(email)){
+        throw new ApiError(400, "Email is required, Please enter valid email")
+    }
+    if(!password || !isValidPassword(password)){
+        throw new ApiError(400, "Password is required, Please enter valid password")
+    }
+    const existingUser = await User.findOne({ $or: [{ email }, { userName }] })
+    if(existingUser){
+        if(existingUser.email === email){
+            throw new ApiError(409, "Email already in use, Please login or use different email")
+        }
+        if(existingUser.userName === userName){
+            throw new ApiError(409, "User name already in use, Please choose a different user name")
+        }
+    }
+    const userCreated = await User.create({ fullName, userName, email, password }).select("-password -refreshToken")
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, userCreated, "User registered successfully"))
 })
