c8fb426779dd9ec35c8ddd9c528175938b455600
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-04-12 01:12:44 +0530

    feat: add project routes

--- Backend/src/routes/project.routes.js
+++ Backend/src/routes/project.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createProject } from "../controllers/project.controller.js";
+import { createProject, getExploreProjects, getMyProjects, getProjectById, updateProject, deleteProject } from "../controllers/project.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 const router = Router();
 
-router.post("/",verifyToken, createProject);
-
+router.post("/",verifyToken, createProject)
+router.get("/me", verifyToken, getMyProjects)
+router.get("/", verifyToken, getExploreProjects)
+router.patch("/:projectId", verifyToken, updateProject)
+router.delete("/:projectId", verifyToken, deleteProject)
+router.get("/:projectId", verifyToken, getProjectById)
 export default router
