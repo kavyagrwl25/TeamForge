76d11388fe9cb3467bbbcf10633f03ced01eb6d4
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-04-14 01:28:29 +0530

    feat: add request model

---
+++ Backend/src/models/request.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema(
+  {
+    requestedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Requested user is required"],
+    },
+
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: [true, "Project is required"],
+    },
+
+    message: {
+      type: String,
+      trim: true,
+      maxlength: [300, "Message cannot exceed 300 characters"],
+      default: "",
+    },
+
+    roleRequested: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// prevent same user from sending multiple requests to same project
+requestSchema.index({ requestedBy: 1, project: 1 }, { unique: true });
+
+export const Request = mongoose.model("Request", requestSchema);
