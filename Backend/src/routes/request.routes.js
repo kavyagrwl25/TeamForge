d927aafa3811ae070fecbd8ae75f8e067b0f0592
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-04-16 18:38:34 +0530

    feat: add route for updateRequestStatus controller

--- Backend/src/routes/request.routes.js
+++ Backend/src/routes/request.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/auth.middleware.js";
-import { createRequest, getMySentRequests, getRequestsForMyProject } from "../controllers/request.controller.js";
+import { createRequest, getMySentRequests, getRequestsForMyProject, updateRequestStatus } from "../controllers/request.controller.js";
 
 const router = Router();
 
 router.get("/project/me", verifyToken, getMySentRequests)
 router.get("/project/:projectId", verifyToken, getRequestsForMyProject)
 router.post("/project/:projectId", verifyToken, createRequest)
+router.patch("/:requestId/status", verifyToken, updateRequestStatus)
 
 export default router
